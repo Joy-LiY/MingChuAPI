9173c911ff17701f0aff114d03980d18fefa9a74
wangzaizaizaizai <jiaxin_li_2019@163.com>
2023-01-30 09:14:52 +0800

    B01 J

--- src/main/java/com/example/mingchuapi/service/LiveService.java
+++ src/main/java/com/example/mingchuapi/service/LiveService.java
@@ -1,13 +1,23 @@
 package com.example.mingchuapi.service;
 
+import com.alibaba.fastjson.JSONObject;
 import com.example.mingchuapi.model.LiveModel;
+import com.example.mingchuapi.util.HttpUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LiveService {
 
+    HttpUtils httpUtils;
+
     public LiveModel getLiveUrl(String url, String deviceId, long timestamp) {
         LiveModel liveModel = new LiveModel();
+        liveModel.setDeviceId(deviceId);
+        if (timestamp != 0) {
+            liveModel.setEndTime(timestamp);
+        }
+        String liveurl = httpUtils.sendPost(url, JSONObject.toJSONString(liveModel));
         return  liveModel;
     }
 
