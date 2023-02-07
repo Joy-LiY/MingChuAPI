069b8e0207c6c10cfc890587fc3b2b0381ff3540
taozi <1449434695@qq.com>
2023-02-07 14:29:50 +0800

    [提交说明] 获取设备历史回放本地视频地址接口,参数获取修改

--- src/main/java/com/example/mingchuapi/service/LiveService.java
+++ src/main/java/com/example/mingchuapi/service/LiveService.java
@@ -1,24 +1,18 @@
 package com.example.mingchuapi.service;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.example.mingchuapi.enums.AndmuCode;
 import com.example.mingchuapi.model.CodeEnum;
-import com.example.mingchuapi.model.LiveModel;
 import com.example.mingchuapi.model.Result;
-import com.example.mingchuapi.model.UrlModel;
 import com.example.mingchuapi.util.AndmuUtils;
-import com.example.mingchuapi.util.HttpUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.SortedMap;
 import java.util.TreeMap;
@@ -109,7 +103,7 @@ public class LiveService {
                 result.setResultMsg(CodeEnum.RESULT_CODE_SUCCESS.getMsg());
 
                 HashMap<String, String> map = new HashMap<>();
-                map.put("url", resultJson.getJSONObject("data").getString("url"));
+                map.put("url", resultJson.getJSONObject("data").getString("hlsUrl"));
 
                 result.setData(map);
             } else {
